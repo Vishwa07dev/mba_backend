2ae41fd384cd53dcd8f37840b70c76e8ec5516c1
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-04-10 16:42:52 +0530

    added the code for session 6

--- routes/theatre.routes.js
+++ routes/theatre.routes.js
@@ -1,5 +1,5 @@
 const theatreController = require("../controllers/theatre.controller");
-const { verifyTheatreReqBody } = require("../middlewares");
+const { authJwt, verifyTheatreReqBody } = require("../middlewares");
 
 
 
@@ -8,11 +8,11 @@ const { verifyTheatreReqBody } = require("../middlewares");
  */
 
 module.exports = function (app) {
-    app.get("/mba/api/v1/theatres", theatreController.getAllTheatres);
-    app.get("/mba/api/v1/theatres/:id", theatreController.getTheatre);
-    app.post("/mba/api/v1/theatres",[verifyTheatreReqBody.validateTheatreRequestBody], theatreController.createTheatre);
-    app.put("/mba/api/v1/theatres/:id",[verifyTheatreReqBody.validateTheatreRequestBody], theatreController.updateTheatre);
-    app.delete("/mba/api/v1/theatres/:id", theatreController.deleteTheatre);
-    app.put("/mba/api/v1/theatres/:id/movies", theatreController.addMoviesToATheater); 
-    app.get("/mba/api/v1/theatres/:theatreId/movies/:movieId", theatreController.checkMovieInsideATheatre);   
+    app.get("/mba/api/v1/theatres", [authJwt.verifyToken], theatreController.getAllTheatres);
+    app.get("/mba/api/v1/theatres/:id", [authJwt.verifyToken], theatreController.getTheatre);
+    app.post("/mba/api/v1/theatres", [authJwt.verifyToken, authJwt.isAdmin, verifyTheatreReqBody.validateTheatreRequestBody], theatreController.createTheatre);
+    app.put("/mba/api/v1/theatres/:id", [authJwt.verifyToken, authJwt.isAdmin, verifyTheatreReqBody.validateTheatreRequestBody], theatreController.updateTheatre);
+    app.delete("/mba/api/v1/theatres/:id", [authJwt.verifyToken, authJwt.isAdmin], theatreController.deleteTheatre);
+    app.put("/mba/api/v1/theatres/:id/movies", [authJwt.verifyToken, authJwt.isAdmin], theatreController.addMoviesToATheater);
+    app.get("/mba/api/v1/theatres/:theatreId/movies/:movieId", [authJwt.verifyToken], theatreController.checkMovieInsideATheatre);
 }
